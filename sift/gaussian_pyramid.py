545720f5c424084c3050674fc4f6d31ba0f01596
AquanJSW <zhdlcc@gmail.com>
2020-07-20 09:31:19 +0800

    works but has edge problem

---
+++ sift/gaussian_pyramid.py
@@ -0,0 +1,148 @@
+"""Gaussian Pyramid
+"""
+
+import argparse
+import numpy as np
+import cv2
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+from split_gaussian_blur import GaussianBlurNet
+import matplotlib.pyplot as plt
+import time
+
+parser = argparse.ArgumentParser(description="Generate gaussian pyramid")
+parser.add_argument('-p', '--path', default="./549.jpg", type=str, help="image's path")
+parser.add_argument('-S', default=3, type=int, help="interval layers number = S + 3")
+parser.add_argument('-s0', '--sigma0', default=1.6, type=float, help="the value of sigma_0")
+parse = parser.parse_args()
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+class GaussianPyramid:
+    def __init__(self, path=parse.path, S=parse.S, sigma0=parse.sigma0):
+        self.img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+        self.S = S  # 高斯金字塔的组内层数 = S + 3
+        self.sigma0 = sigma0  # 初始高斯模糊的方差
+        self.O = int(np.log2(self.img.shape).min())     # 高斯金字塔组数
+        net = GaussianPyramidNet(img=self.img, S=self.S, sigma0=self.sigma0, O=self.O)
+        self.pyramid = net()
+
+    def get(self):
+        """返回张量形式的高斯金字塔
+        """
+        return self.pyramid
+
+    def show(self):
+        pyramid = self._to_numpy()
+        num = self.S + 3
+        for i, img in enumerate(pyramid):
+            plt.figure(i // num)
+            plt.subplot(num, 1, i % num + 1), plt.imshow(img, 'gray')
+        plt.show()
+
+    def write(self):
+        t = time.ctime(time.time())
+        pyramid = self._to_numpy()
+        for i, img in enumerate(pyramid):
+            o = i // self.O
+            s = i % self.O
+            cv2.imwrite('./output/o%ds%d-%s.jpg' % (o, s, t), img)
+
+    def _to_numpy(self):
+        pyramid_ = list()
+        for img in self.pyramid:
+            pyramid_.append(img.to(device='cpu', dtype=torch.uint8).squeeze().numpy())
+        return pyramid_
+
+
+class GaussianPyramidNet(nn.Module):
+    """该网络直接生成高斯金字塔
+    """
+    def __init__(self, img, S, sigma0, O):
+        super(GaussianPyramidNet, self).__init__()
+        self.img = img
+        self.S = S
+        self.sigma0 = sigma0
+        self.O = O
+        self.one = torch.tensor([[[[1.]]]]).to(device=device, dtype=torch.float)
+
+    def forward(self):
+        pyramid = list()
+        pyramid.append(preprocess(self.img))
+
+        # 生成组内尺度坐标
+        k = np.power(2, 1 / self.S)
+        sigma = list()
+        for s in range(1, self.S + 3):
+            pre = self.sigma0 * (k ** (s - 1))
+            now = pre * k
+            sigma.append((now ** 2 - pre ** 2) ** 0.5)
+
+        for o in range(self.O):
+            """组循环
+            """
+            if o == 0:
+                pass
+            else:
+                """新组的第一张图像由上一组第三张图像隔点下采样得到
+                """
+                pyramid.append(F.conv2d(input=pyramid[(o - 1) * (self.S + 3) + 2],
+                                        weight=self.one, stride=2))
+            for s in range(1, self.S + 3):
+                """层循环
+                对上一张图像进行卷积
+                """
+                net = GaussianBlurNet_(compute_kernel(sigma[s - 1]))
+                pyramid.append(net(pyramid[o * (self.S + 3) + s - 1]))
+
+        return pyramid
+
+
+class GaussianBlurNet_(GaussianBlurNet):
+    def __init__(self, kernel):
+        super(GaussianBlurNet_, self).__init__(kernel)
+
+    def forward(self, img):
+        """
+        :param img: type is np or tensor
+        :return: type is tensor
+        """
+        if not torch.is_tensor(img):
+            img = torch.from_numpy(img).to(device=device, dtype=torch.float)
+            img = img.unsqueeze(0).unsqueeze(0)
+        """Reflect padding~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        
+        """
+        # img = F.pad(img, [0, 0, self._padding, self._padding], mode='reflect')
+        img = F.conv2d(img, weight=self._vkernel, padding=(self._padding, 0))
+        img = F.conv2d(img, weight=self._hkernel, padding=(0, self._padding))
+        return img
+
+
+def compute_kernel(sigma):
+    """计算高斯卷积核大小
+    """
+    kernel_size = int(sigma * 6 + 1)
+    kernel_size = kernel_size if kernel_size % 2 == 1 else kernel_size + 1
+    kernel = cv2.getGaussianKernel(kernel_size, sigma)
+    return kernel
+
+
+def preprocess(img):
+    """图像预处理
+    1 高斯模糊抗混淆
+    2 双线性插值放大一倍
+    :param img: type is np
+    :return: type is tensor
+    """
+    sigma = 0.5  # 抗击混淆所用的高斯模糊方差
+    net = GaussianBlurNet_(compute_kernel(sigma))
+    img = net(img)
+    img = F.interpolate(img, scale_factor=2, mode='bilinear')
+    return img
+
+if __name__ == '__main__':
+    t = GaussianPyramid()
+    t()
--- sift/split_gaussian_blur.py
+++ sift/split_gaussian_blur.py
@@ -1,6 +1,8 @@
 """
 Gaussian Blur using openCV and gpu acceleration
 Note: Split Gaussian Kernel is used
+
+API version
 """
 
 import torch
@@ -8,31 +10,20 @@ import torch.nn as nn
 import torch.nn.functional as F
 import cv2
 import matplotlib.pyplot as plt
-import argparse
 
-path = './549.jpg'
-
-parser = argparse.ArgumentParser(description='Split Gaussian Blur')
-parser.add_argument('-p', '--path', default=path, type=str, help="image's path")
-parser.add_argument('-k', '--kernel_size', default=3, type=int, help="kernel size, must be an odd")
-parser.add_argument('-s', '--sigma', default=1.0, type=float, help="value of sigma")
-parser.add_argument('-nc', '--no_cuda', default=False, action='store_const', const=True,
-                    help="no CUDA once specified")
-parse = parser.parse_args()
-
-device = torch.device('cuda' if (not parse.no_cuda) and torch.cuda.is_available() else 'cpu')
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
-# 用于高斯模糊
-class _GaussianBlurNet(nn.Module):
-    """Do not use this class
-    """
+class GaussianBlurNet(nn.Module):
     def __init__(self, kernel):
-        super(_GaussianBlurNet, self).__init__()
+        """
+        :param kernel: from cv2.getGaussianKernel()
+        """
+        super(GaussianBlurNet, self).__init__()
         self.train(False)
         # 两个一维卷积核
         # 可以不使用nn.Parameter，因为该方法将v/hkernel转化为可训练的参数，但这里根本不用训练
-        vkernel = torch.from_numpy(kernel).to(device=device, dtype=torch.float16)
+        vkernel = torch.from_numpy(kernel).to(device=device, dtype=torch.float)
         hkernel = vkernel.T
         self._vkernel = vkernel.unsqueeze(0).unsqueeze(0)
         self._hkernel = hkernel.unsqueeze(0).unsqueeze(0)
@@ -40,10 +31,10 @@ class _GaussianBlurNet(nn.Module):
 
     # 两次卷积实现分离高斯模糊
     def forward(self, img):
-        img = torch.from_numpy(img).to(device=device, dtype=torch.float16)
+        img = torch.from_numpy(img).to(device=device, dtype=torch.float)
         img = img.unsqueeze(0).unsqueeze(0)
-        img = F.conv2d(img, weight=self._vkernel, padding=(0, self._padding))
-        img = F.conv2d(img, weight=self._hkernel, padding=(self._padding, 0))
+        img = F.conv2d(img, weight=self._vkernel, padding=(self._padding, 0))
+        img = F.conv2d(img, weight=self._hkernel, padding=(0, self._padding))
         img = img.squeeze().to(device='cpu', dtype=torch.uint8).numpy()
         return img
 
@@ -51,15 +42,15 @@ class _GaussianBlurNet(nn.Module):
 class GaussianBlur:
     """Use this class
     """
-    def __init__(self, path=parse.path, kernel_size=parse.kernel_size, sigma=parse.sigma):
+    def __init__(self, img, kernel_size, sigma):
         # 核心非奇数返回Error
         if not (kernel_size % 2):
             raise ValueError("kernel size must be and odd")
         self.kernel_size = kernel_size
         self.sigma = sigma
-        self.img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+        self.img = img
         self.kernel = cv2.getGaussianKernel(kernel_size, sigma)
-        net = _GaussianBlurNet(self.kernel)
+        net = GaussianBlurNet(self.kernel)
         self.out = net(self.img)
 
     def show(self):
