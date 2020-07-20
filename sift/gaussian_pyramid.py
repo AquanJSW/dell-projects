bf880c3bac12166d5da7ea5a73eb58de5ef71823
AquanJSW <zhdlcc@gmail.com>
2020-07-20 14:02:08 +0800

    phase 1

--- sift/gaussian_pyramid.py
+++ sift/gaussian_pyramid.py
@@ -25,39 +25,82 @@ class GaussianPyramid:
     注意计算高斯金字塔组数时，考虑组内最后一张图像的高斯模板大小k，金字塔顶层的图像大小应 >= odd(k / 2)
     """
     def __init__(self, path=parse.path, S=parse.S, sigma0=parse.sigma0):
+        """
+        :param path: image's path.
+        :param S: Gaussian Feature Maps' number of each octave.
+        :param sigma0: initial sigma.
+        :type [str, int, float]
+        """
         self.img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
-        self.S = S  # 高斯金字塔的组内层数 = S + 3
+        self.S = S  # Gaussian Feature Maps' number per octave
+        self.Sp3 = S + 3    # Number of Images of each octave in Gaussian Pyramid
         self.sigma0 = sigma0  # 初始高斯模糊的方差
         self.O = int(np.log2(self.img.shape).min() - np.log2(16)) + 1    # 高斯金字塔组数
         net = GaussianPyramidNet(img=self.img, S=self.S, sigma0=self.sigma0, O=self.O)
         self.pyramid = net()
 
-    def get(self):
-        """返回张量形式的高斯金字塔
+    def get_gaussian_pyramid(self):
+        """返回高斯金字塔
+        :return: list of tensors, list[0] is initial layer's tensor , len(list) = 组数 * 组内层数
+            each tensor's shape is [1, 1, H, W]
         """
         return self.pyramid
 
-    def show(self):
-        pyramid = self._to_numpy()
+    def get_DoG(self):
+        """返回高斯差分金字塔(Difference of Gaussian)
+        :return list of tensors with shape [1, 1, H, W]
+        """
+        DoG = list()
+        for i in range(len(self.pyramid)):
+            if i % self.Sp3 == 0:
+                """每组首张图像不计算DoG
+                """
+                pass
+            else:
+                DoG.append(self.pyramid[i] - self.pyramid[i - 1])
+        return DoG
+
+    def show_gaussian_pyramid(self):
+        pyramid = self._pyramid_to_numpy(self.pyramid)
         num = self.S + 3
         for i, img in enumerate(pyramid):
             plt.figure(i // num)
             plt.subplot(num, 1, i % num + 1), plt.imshow(img, 'gray')
         plt.show()
 
-    def write(self):
-        """输出为图像
+    def save_DoG(self):
+        """保存为图像
+        注意，这个图像并不严谨，仅供可视化，因为归一化在检测极值点过程中没有意义
         """
         t = time.ctime(time.time())
-        pyramid = self._to_numpy()
-        for i, img in enumerate(pyramid):
-            o = i // self.O
-            s = i % self.O
-            cv2.imwrite('./output/o%ds%d-%s.jpg' % (o, s, t), img)
+        DoG = self.get_DoG()
+        for i in range(len(DoG)):
+            DoG[i] = DoG[i].to(device='cpu').squeeze().numpy()
+        Sp2 = self.S + 2
+        for i, img in enumerate(DoG):
+            o = i // Sp2
+            s = i % Sp2
+            range_ = [img.min(), img.max()]
+            if range_[0] < 0:
+                img -= range_[0]
+                range_ -= range_[0]
+            img = np.asarray(img * 255 / (range_[1] - range_[0]), dtype=int)    # 归一化并向下取整
+            cv2.imwrite('./output/DoG-o%ds%d-%s.jpg' % (o, s, t), img)
 
-    def _to_numpy(self):
+    def save_gaussian_pyramid(self):
+        """保存为图像
+        """
+        t = time.ctime(time.time())
+        pyramid = self._pyramid_to_numpy(self.pyramid)
+        for i, img in enumerate(pyramid):
+            o = i // self.Sp3   # 0组其实就是-1组
+            s = i % self.Sp3
+            cv2.imwrite('./output/gaussian-pyramid-o%ds%d-%s.jpg' % (o, s, t), img)
+
+    @staticmethod
+    def _pyramid_to_numpy(pyramid):
         pyramid_ = list()
-        for img in self.pyramid:
+        for img in pyramid:
             pyramid_.append(img.to(device='cpu', dtype=torch.uint8).squeeze().numpy())
         return pyramid_
 
@@ -69,6 +112,7 @@ class GaussianPyramidNet(nn.Module):
         super(GaussianPyramidNet, self).__init__()
         self.img = img
         self.S = S
+        self.Sp3 = S + 3
         self.sigma0 = sigma0
         self.O = O
         self.one = torch.tensor([[[[1.]]]]).to(device=device, dtype=torch.float)
@@ -93,14 +137,14 @@ class GaussianPyramidNet(nn.Module):
             else:
                 """新组的第一张图像由上一组倒数第三张图像隔点下采样得到
                 """
-                pyramid.append(F.conv2d(input=pyramid[o * (self.S + 3) - 3],
+                pyramid.append(F.conv2d(input=pyramid[o * self.Sp3 - 3],
                                         weight=self.one, stride=2))
             for s in range(1, self.S + 3):
                 """层循环
                 对上一张图像进行卷积
                 """
                 net = GaussianBlurNet_(compute_kernel(sigma[s - 1]))
-                pyramid.append(net(pyramid[o * (self.S + 3) + s - 1]))
+                pyramid.append(net(pyramid[o * self.Sp3 + s - 1]))
 
         return pyramid
 
@@ -119,9 +163,7 @@ class GaussianBlurNet_(GaussianBlurNet):
         if not torch.is_tensor(img):
             img = torch.from_numpy(img).to(device=device, dtype=torch.float)
             img = img.unsqueeze(0).unsqueeze(0)
-        """Reflect padding~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        
-        """
+        # reflect padding 才不会卷积出黑色边缘
         padH = nn.ReflectionPad2d((0, 0, self._padding, self._padding))
         padW = nn.ReflectionPad2d((self._padding, self._padding, 0, 0))
         img = padH(img)
---
+++ sift/test.py
@@ -0,0 +1,6 @@
+#!python3
+
+from gaussian_pyramid import GaussianPyramid
+g = GaussianPyramid()
+g.save_DoG()
+g.save_gaussian_pyramid()
